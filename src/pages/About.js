259f789afc3137a5989f76d0b49450a9977deb60
Robin Burrage <robin.o.burrage@gmail.com>
2022-12-11 18:02:46 -0500

    Initial Commit

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil minus incidunt saepe! Accusamus aspernatur ducimus sequi, laboriosam nam eligendi aut dignissimos, reprehenderit deserunt, soluta ipsa delectus architecto fuga repudiandae aliquam omnis odio dicta aliquid magni perspiciatis commodi velit! Nobis, saepe.</p>
+    </section>
+  )
+}
+
+export default About
